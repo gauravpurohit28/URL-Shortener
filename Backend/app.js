8b4f52aa17335f0b14e26535ae427c188af51cf9
Gaurav Purohit <grajpurohittt@gmail.com>
2025-08-20 22:46:33 +0530

    nanoid update

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,20 @@
 import express from "express"
-
+import {nanoid} from "nanoid"
 const app = express()
 
-const PORT=5000
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
+app.post("/api/create",(req,res)=>{
+    const {url} = req.body
+    res.send(nanoid(7))
+})
+
+
+
+
+
+const PORT=5000
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}` );
 })
